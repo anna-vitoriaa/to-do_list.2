c8ec0118b827e9bbb7303b772b7e78bee6883517
Anna Vitoria <vitoria.963369@gmail.com>
2025-09-17 09:31:50 -0300

    Cria tabela Tarefas no banco de dados

--- database.py
+++ database.py
@@ -1,3 +1,12 @@
 import sqlite3
 conn = sqlite3.connect("To-do_list.db")
 crs = conn.cursor()
+
+crs.execute('''
+    CREATE TABLE IF NOT EXISTS Tarefas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        data TEXT NOT NULL,
+        situacao INTEGER DEFAULT 0)''')
+
+conn.commit()
